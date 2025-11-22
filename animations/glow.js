af68fa41cde9ee6a5da6ef2f343073a4e69a7bbc
j6fgfhzwf2-ship-it <j6fgfhzwf2@privaterelay.appleid.com>
2025-11-22 19:24:56 +0100

    Update glow.js

--- animations/glow.js
+++ animations/glow.js
@@ -1,5 +1,5 @@
-// glow.js
-export function drawGlow(context) {
-    // Code pour effet glow
+export function drawGlow(ctx) {
+    ctx.strokeStyle = 'yellow';
+    ctx.lineWidth = 5;
+    ctx.strokeRect(100, 100, 200, 200);
 }
-
