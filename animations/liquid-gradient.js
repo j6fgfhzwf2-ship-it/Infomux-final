2feeadf0b291723bc31a73cb6825fcb19a1a6e52
j6fgfhzwf2-ship-it <j6fgfhzwf2@privaterelay.appleid.com>
2025-11-22 19:25:16 +0100

    Update liquid-gradient.js

--- animations/liquid-gradient.js
+++ animations/liquid-gradient.js
@@ -1,5 +1,7 @@
-// liquid-gradient.js
-export function drawLiquidGradient(context) {
-    // Code pour gradient liquide
+export function drawLiquidGradient(ctx) {
+    const gradient = ctx.createLinearGradient(0, 0, 800, 600);
+    gradient.addColorStop(0, 'blue');
+    gradient.addColorStop(1, 'purple');
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, 800, 600);
 }
-
