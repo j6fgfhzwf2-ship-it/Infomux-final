df67a55ac8112cad02b0e0fe369d4b69a547db60
j6fgfhzwf2-ship-it <j6fgfhzwf2@privaterelay.appleid.com>
2025-11-22 18:31:27 +0100

    Expose electronAPI for communication with renderer

--- electron-app/preload.js
+++ electron-app/preload.js
@@ -1,1 +1,8 @@
+// preload.js
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    sendMessage: (channel, data) => ipcRenderer.send(channel, data),
+    onMessage: (channel, callback) => ipcRenderer.on(channel, callback)
+});
 
